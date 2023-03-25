3c8352b15572c25b1dd1d639bd835e9863d64863
Ali-Nawab <alinawabkhan15@gmail.com>
2023-03-25 13:51:29 +0500

    gufranullahs has been changed to gufranullah

--- Task 9/guest_book.py
+++ Task 9/guest_book.py
@@ -3,6 +3,7 @@ they enter their name, print a greeting to the screen and add a line recording
 their visit in a file called guest_book.txt. Make sure each entry appears on a 
 new line in the file."""
 
+"""
 while True: # loop until the user enters 'quit'.
     name = input("Please enter your name: ") # prompt the user for their name.
     if name == 'quit': # if the user enters 'quit', break out of the loop.
@@ -13,3 +14,17 @@ while True: # loop until the user enters 'quit'.
             print("hello " + name + "! Your name has been added to the guest book.") # print a greeting to the screen.
             
             
+"""
+        
+# Read the file contents
+with open('guest_book.txt', 'r') as file:
+    contents = file.read()
+
+# Modify the desired word
+contents = contents.replace('Gufranullahhas', 'gufranullah')
+
+# Write the modified contents back to the file
+with open('guest_book.txt', 'w') as file:
+    file.write(contents)
+
+    
